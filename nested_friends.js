ef383b1bd1139848a3bcd5a3cf7b7940be31d501
DonkeYSpotteD <alienjiad@gmail.com>
2026-01-25 23:08:32 +0800

    commit-2

--- nested_friends.js
+++ nested_friends.js
@@ -1,20 +1,23 @@
-let myScore= 85;
-let friendScore= 70;
-if(myScore >=80 ){
-    if(friendScore >=80   )
+let myScore= 78;
+let friendScore=39;
+if(myScore >=80)
+{
+    if(friendScore >=80)
     {
-        console.log("Go for a lunch ");
+        console.log(" go for a lunch");
     }
-    else if ( friendScore <=80 && friendScore >=60 ){
-        console.log("tell your friend, good luck next time");
+    else if ( friendScore >=60 && friendScore <80)
+    {
+        console.log(" good luck next time");
     }
-    else if (friendScore <=60 && friendScore >=40 ){
-        console.log("keep your friend's message unseen");
+    else if (friendScore <=60 && friendScore >40)
+    {
+       console.log("keep your friend's message unseen");
     }
-    else{
-        console.log("Block your friend" );
+    else {
+        console.log("Block your friend");
     }
 }
-else{
-    console.log(" go to home and sleep and act sad");
-}   
+else {
+    console.log("go to home and sleep and act sad");
+}
---
+++ ternery_number.js
@@ -0,0 +1,5 @@
+let num1 = 4;
+let num2 = 5;
+let result;
+result = (num1>num2) ? (num1*2) : (num1+num2);
+console.log(result);
